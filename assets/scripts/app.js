bd27e7fcf2353c1bb021cb2a8d308c5a8ebef61a
gajerabiren <gajerabiren23@gmail.com>
2021-02-25 11:05:34 +0530

    more-functions-added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
+//get input from input field
+
 function getUserInput(){
     return parseInt(userInput.value)
 }
@@ -10,42 +12,111 @@ function getUserInputTwo() {
     return parseInt(userInputTwo.value)
 }
 
+//generates and write calculation logs
+
 function createAndLog(operator, resultBeforeCalc, calcNumber){
     const calcDescription = `${resultBeforeCalc}${operator}${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); // from vendor file
 }
 
+//addition function
 function add() {
     const enteredNumber = getUserInput();
     const enteredNumberTwo = getUserInputTwo();
+
     
-    currentResult = enteredNumber + enteredNumberTwo;
-    createAndLog('+' , enteredNumberTwo , enteredNumber);
+
+    if( enteredNumber === 0 && enteredNumberTwo === 0) {
+        currentCalculationOutput.textContent = "";
+        currentResultOutput.textContent = 0;
+        
+
+    } else {
+        currentResult = enteredNumber + enteredNumberTwo;
+        createAndLog('+' , enteredNumber , enteredNumberTwo);
+        
+    } 
+
+    
     
 }
+
+// substraction function
 
 function substract() {
     const enteredNumber = getUserInput();
-    const initialResult = currentResult;
+    const enteredNumberTwo = getUserInputTwo();
     
-    currentResult = currentResult - enteredNumber;
-    createAndLog('-' , initialResult , enteredNumber);
+    if( enteredNumber === 0 && enteredNumberTwo === 0) {
+        currentCalculationOutput.textContent = "";
+        currentResultOutput.textContent = 0;
+        
+
+    } else {
+        currentResult = enteredNumber - enteredNumberTwo;
+        createAndLog('-' , enteredNumber , enteredNumberTwo);
+        
+    }
 }
+
+// multiplication function
 
 function multiply(){
     const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
-    createAndLog('*' , initialResult , enteredNumber);
+    const enteredNumberTwo = getUserInputTwo();
+    
+    if( enteredNumber === 0 && enteredNumberTwo === 0) {
+        currentCalculationOutput.textContent = "";
+        currentResultOutput.textContent = 0;
+        
+
+    } else {
+        currentResult = enteredNumber * enteredNumberTwo;
+        createAndLog('*' , enteredNumber , enteredNumberTwo);
+        
+    }
 }
+
+//division function
 
 function divide(){
     const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
-    createAndLog('/' , initialResult , enteredNumber);
+    const enteredNumberTwo = getUserInputTwo();
+    
+    if( enteredNumber === 0 && enteredNumberTwo === 0) {
+        currentCalculationOutput.textContent = "";
+        currentResultOutput.textContent = 0;
+        
+
+    } else {
+        currentResult = enteredNumber / enteredNumberTwo;
+        createAndLog('/' , enteredNumber , enteredNumberTwo);
+        
+    }
 }
 
+
+//function results
+
+function equal(){
+    resultDisplay.style.display = "block";
+}
+
+//function allclear
+
+function allclear(){
+    userInput.value = 0 ;
+    userInputTwo.value = 0;
+    currentCalculationOutput.textContent = "";
+    currentResultOutput.textContent = 0;
+    resultDisplay.style.display = "none";
+    
+}
+
+
+
+equalTwo.addEventListener('click' , equal);
+allClearBtn.addEventListener('click', allclear);
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', substract);
 multiplyBtn.addEventListener('click', multiply);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -6,9 +6,17 @@ const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
-
+const allClearBtn = document.getElementById('all-clear');
+const equalTwo = document.getElementById('equalto');
+const resultDisplay = document.getElementById('results')
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
+
+function validateNumber(evt){
+  const pattern = /^[0-9]$/;
+
+  return pattern.test(evt.key)
+}
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
